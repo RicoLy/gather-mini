21d2d76b5cd47070142d7fa0718c7ff2a3dc80f2
v_ricolyluo <v_ricolyluo@tencent.com>
2022-06-22 18:14:21 +0800

    ''

---
+++ pages/map/map.vue
@@ -0,0 +1,65 @@
+<template>
+	<view class="content">
+		<map :markers="markers" 
+			 @markertap="btnMarkerTap" 
+			 :latitude="latitude" 
+			 :longitude="longitude" 
+			 :show-location="true" 
+			 @regionchange="onRegionChanged" 
+			 style="width: 100%;height: 100%;"></map>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			uni.getLocation({
+				success: (res) => {
+					this.latitude=res.latitude;
+					this.longitude=res.longitude;
+				}
+			})
+		},
+		methods: {
+			btnMarkerTap(e){
+				console.log(e);
+			},
+			onRegionChanged(e){
+				console.log(e);
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,7 +1,7 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png" @click="reqs()"></image>
-		<view class="text-area">
+		<image style="border-radius:50%" class="logo" src="/static/icon_dog.jpg"></image>
+		<view>
 			<text class="title">{{title}}</text>
 		</view>
 	</view>
@@ -18,14 +18,7 @@
 
 		},
 		methods: {
-			reqs() {
-				uni.request({
-					url: 'www.baidu.com',
-					success: (res) => {
-						console.log(res);
-					}
-				})
-			}
+
 		}
 	}
 </script>
@@ -41,10 +34,7 @@
 	.logo {
 		height: 200rpx;
 		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
+		margin: 200rpx auto 50rpx auto;
 	}
 
 	.text-area {
