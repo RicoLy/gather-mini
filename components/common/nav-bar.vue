598408228016529f936ae09a8f4b96c0ee4d366f
v_ricolyluo <v_ricolyluo@tencent.com>
2022-06-27 18:25:12 +0800

    ''

---
+++ components/common/nav-bar.vue
@@ -0,0 +1,42 @@
+<template>
+	<view class="fixed-top bg-white">
+		<uni-status-bar></uni-status-bar>
+		<view style="height: 44px;" class="flex border-bottom align-center">
+			<view class="flex-1">
+				<slot name="left"></slot>
+			</view>
+			<view class="flex-1 flex justify-center">
+				<slot name="center"></slot>
+			</view>
+			<view class="flex-1 flex justify-end">
+				<slot name="right"></slot>
+			</view>
+		</view>
+		
+		<!-- 占位 -->
+		<view>
+			<uni-status-bar></uni-status-bar>
+			<view style="height: 44px;"></view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	import uniStatusBar from '@/components/uni-ui/uni-status-bar/uni-status-bar.vue';
+	export default {
+		components: {
+			uniStatusBar
+		},
+		name:"nav-bar",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<view>
+		<!-- 导航 -->
+		<!-- <nav-bar>
+			<text slot='left' class="font-md ml-3">首页</text>
+			<template slot='right' class='flex'>
+				<view style="width: 60rpx;height: 60rpx;"
+				class="flex align-center justify-center bg-light rounded-circle mr-3" hover-class="bg-hover-light">
+					<text class="iconfont icon-zengjia"></text>
+				</view>
+				<view style="width: 60rpx;height: 60rpx;"
+				class="flex align-center justify-center bg-light rounded-circle mr-3" hover-class="bg-hover-light">
+					<text class="iconfont icon-gengduo"></text>
+				</view>
+			</template>
+		</nav-bar> -->
+		
+		<!-- 搜索框 -->
+		<!-- 搜索框 -->
+		<view class="px-3 py-2">
+			<view class="position-relative">
+				<view style="height: 70rpx;width: 70rpx;position: absolute;top: 0;left: 0;" class="flex align-center justify-center text-light-muted">
+					<text class="iconfont icon-sousuo"></text>
+				</view>
+				<input style="height: 70rpx;padding-left: 70rpx;" type="text" class="bg-light font-md rounded-circle" placeholder="搜索" />
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	// import navBar from '@/components/common/nav-bar.vue';
+	export default {
+		components: {
+			// navBar
+		},
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
